0ec9d2eb4e6beb5599be64360ea2df570515645f
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2023-03-14 15:19:05 +0100

    Finished PythonModule01/ex02

--- Python-Module-00/ex10/loading.py
+++ Python-Module-00/ex10/loading.py
@@ -7,9 +7,9 @@ def ft_progress(lst):
         perc = (100 * i) / len(lst)
         if perc % 2 == 0:
             bar += "="
-        print("ETA: {:.2f}s [{:3.0f}%][{: <51}] {}/{} | elapsed time {:.2f}s".format(7.82, perc, bar + ">", i + 1, len(lst), 2.5), end="\r")
+        print("ETA: {:.2f}s [{:3.0f}%][{: <51}] {}/{} | elapsed time {:.2f}s".format(7.82, perc, bar + ">", i + 1, len(lst), 2.5), end='\r')
         yield i
-    print()
+    # print()
 
 listy = range(1000)
 ret = 0
--- Python-Module-01/ex02/vector.py
+++ Python-Module-01/ex02/vector.py
@@ -1,35 +1,116 @@
 class   Vector:
     def __init__(self, vec) -> None:
-        self.values = []
-        self.shape = ()
+        if type(vec) == int:
+            self.shape = (vec, 1)
+            self.values = []
+            for i in range(vec):
+                self.values.append([float(i)])
+        elif type(vec) == list:
+            self.values = vec
+            self.shape = (len(vec), len(vec[0]))
+            assert self.shape[0] == 1 or self.shape[1] == 1, 'Invalid vector'
+        elif type(vec) == tuple:
+            assert vec[1] > vec[0], 'Invalid tuple to create vector'
+            self.shape = (vec[1] - vec[0], 1)
+            self.values = []
+            for i in range(vec[0], vec[1]):
+                self.values.append([float(i)])
 
-    def dot(self):
+    def dot(self, other):
         '''Returns the dot product of two veectors of the same shape'''
+        assert self.shape == other.shape, 'Not able to dot product between vectors with different shapes'
+        dot_product = 0
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                dot_product += self.values[i][j] * other.values[i][j]
+        return dot_product
 
     def T(self):
         '''Transposes the vector from a column vector to a row vector and viceversa'''
+        transposed_matrix = [[0 for _ in range(self.shape[0])] for _ in range(self.shape[1])]
 
-    def __add__(self):
-        ''''''
-    def __radd__(self):
-        ''''''
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                transposed_matrix[j][i] = self.values[i][j]
+        return Vector(transposed_matrix)
 
-    def __sub__(self):
-        ''''''
-    def __rsub__(self):
-        ''''''
+    def __add__(self, other):
+        assert self.shape == other.shape, 'Not able to dot product between vectors with different shapes'
 
-    def __truediv__(self):
-        ''''''
-    def __rtruediv__(self):
-        ''''''
+        m = [[0 for _ in range(self.shape[1])] for _ in range(self.shape[0])]
 
-    def __mul__(self):
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                m[i][j] = self.values[i][j] + other.values[i][j]
+        return Vector(m)
+
+
+    def __radd__(self, other):
+        assert self.shape == other.shape, 'Not able to dot product between vectors with different shapes'
+
+        m = [[0 for _ in range(self.shape[1])] for _ in range(self.shape[0])]
+
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                m[i][j] = self.values[i][j] + other.values[i][j]
+        return Vector(m)
+
+    def __sub__(self, other):
+        assert self.shape == other.shape, 'Not able to dot product between vectors with different shapes'
+
+        m = [[0 for _ in range(self.shape[1])] for _ in range(self.shape[0])]
+
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                m[i][j] = self.values[i][j] - other.values[i][j]
+        return Vector(m)
+
+    def __rsub__(self, other):
+        assert self.shape == other.shape, 'Not able to dot product between vectors with different shapes'
+
+        m = [[0 for _ in range(self.shape[1])] for _ in range(self.shape[0])]
+
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                m[i][j] = self.values[i][j] - other.values[i][j]
+        return Vector(m)
+
+
+    def __truediv__(self, n):
         ''''''
-    def __rmul__(self):
+        if n == 0:
+            raise ZeroDivisionError("division by zero.")
+        m = [[0 for _ in range(self.shape[1])] for _ in range(self.shape[0])]
+
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                m[i][j] = self.values[i][j] / n
+        return Vector(m)
+
+    def __rtruediv__(self, n):
         ''''''
+        raise NotImplementedError("Division of a scalar by a Vector is not defined here.")
+
+    def __mul__(self, n):
+        ''''''
+        m = [[0 for _ in range(self.shape[1])] for _ in range(self.shape[0])]
+
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                m[i][j] = self.values[i][j] * n
+        return Vector(m)
+
+    def __rmul__(self, n):
+        ''''''
+        m = [[0 for _ in range(self.shape[1])] for _ in range(self.shape[0])]
+
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                m[i][j] = self.values[i][j] * n
+        return Vector(m)
 
     def __str__(self) -> str:
-        ''''''
+        return f'Vector({self.values})'
     def __repr__(self) -> str:
-        ''''''
+        return f'Vector({self.values})'
+
