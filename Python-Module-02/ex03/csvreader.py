4cd87d749bf694e355ae7cd16c4fa9c1934be5b4
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2023-03-16 11:42:30 +0100

    Done CSV reader

--- Python-Module-02/ex03/csvreader.py
+++ Python-Module-02/ex03/csvreader.py
@@ -5,19 +5,35 @@ class CsvReader():
         self.header = header
         self.skip_top = skip_top
         self.skip_bottom = skip_bottom
+
+        self.header_line = []
+        if self.header:
+            self.header_line = [h.strip('" \n') for h in self.file.readline().split(',')]
     
     def __enter__(self):
-        return self.file
+        return self
 
-    def __exit__(self):
+    def __exit__(self, type, value, traceback):
         self.file.close()
-    
+        return True
+
     def getdata(self):
         """ Retrieves the data/records from skip_top to skip bottom.
         Return:
         nested list (list(list, list, ...)) representing the data.
         """
-        # ... Your code here ...
+        self.file.seek(0)
+        file_contents = self.file.read()
+        lines = file_contents.split('\n')
+        
+        if self.header:
+            lines = lines[1:]
+        lines = lines[self.skip_top:len(lines)-self.skip_bottom]
+
+        data = [[value.strip('" ') for value in line.split(',')] for line in lines]
+        return data
+
+
     def getheader(self):
         """ Retrieves the header from csv file.
         Returns:
@@ -25,12 +41,12 @@ class CsvReader():
         None: (when self.header is False).
         """
         if self.header:
-            pass
+            return self.header_line
         else:
             return None
         
 if __name__ == "__main__":
-    with CsvReader('good.csv') as file:
+    with CsvReader('good.csv', header=True) as file:
         data = file.getdata()
         header = file.getheader()
 
