0b7455f19f2d25abca5afa6dcf76b1fb7772fd9f
rcabezas29 <raul.29cg@gmail.com>
2023-03-20 21:18:54 +0100

    About to finish Module03 - error on multiple dimension array management

--- Python-Module-03/ex04/Kmeans.py
+++ Python-Module-03/ex04/Kmeans.py
@@ -7,16 +7,24 @@ class KmeansClustering:
         self.ncentroid = ncentroid # number of centroids
         self.max_iter = max_iter # number of max iterations to update the centroids
         self.centroids = [] # values of the centroids
+        self.clusters = [[]]
         
     def init_centroids(self, X):
         for _ in range(self.ncentroid):
             self.centroids.append(X[np.random.randint(0, X.__len__() - 1)])
-            
-        print(self.centroids)
         
-    def distance_between_two_points(x, y):
+    def distance_between_two_points(self, x, y):
         return math.sqrt((y[1] - x[1])**2 + (y[2] - x[2])**2 + (y[3] - x[3])**2)
-        
+    
+    def find_the_nearest_centroid(self, person):
+        min_dist = 999999999
+        nearest_centroid = self.centroids[0]
+        for centroid in self.centroids:
+            if min_dist > self.distance_between_two_points(person, centroid):
+                min_dist = self.distance_between_two_points(person, centroid)
+                nearest_centroid = centroid
+        return nearest_centroid
+
     def fit(self, X):
         """
         Run the K-means clustering algorithm.
@@ -33,8 +41,14 @@ class KmeansClustering:
         """
         self.init_centroids(X)
         for _ in range(self.max_iter):
+            for person in X:
+                nearest_centroid = self.find_the_nearest_centroid(person)
+                print(self.centroids)
+                print(np.where(nearest_centroid == self.centroids))
+                self.clusters[].append(person)
+                
+                    
             
-    
     def predict(self, X):
         """
         Predict from wich cluster each datapoint belongs to.
